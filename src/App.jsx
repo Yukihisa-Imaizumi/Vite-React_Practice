3f9e6239ee65f208103a5f8be98bbf0f9756b404
Yukihisa-Imaizumi <s2312328@u.tsukuba.ac.jp>
2025-12-03 21:55:23 +0900

    Material-UIのチュートリアル

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Header from './components/Header'
-import MainLayout from './layouts/MainLayout'
-import Home from './pages/Home'
 import './App.css'
-
-
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import Box from '@mui/material/Box'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
-      <div style={{ marginTop: '60px' }}>
-      <MainLayout>
-        <Home/>
-      </MainLayout> 
-      </div>  
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Typography variant = "h1" mt = {2}>こんにちは</Typography>
+      <HomeIcon />
+      <Box color="text.primary" clone>
+        <Button />
+        <Typography variant = "h1">あいうえお</Typography>
+        <Typography variant = "h2">かきくけこ</Typography>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Box>
     </>
   )
 }
